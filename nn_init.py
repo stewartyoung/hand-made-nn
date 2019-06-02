4f15628767e68696da1df40bee3d33b4be2fc6e9
Stewart Young <stewart@dresser.app>
2019-06-02 10:20:39 +0100

    made neural network class and init method

---
+++ nn_init.py
@@ -0,0 +1,12 @@
+class NeuralNetwork:
+	def __init__(self, x, y):
+		# Take the data x, and assign it to an instance of the input object
+		self.input = x
+		# Produce a first array (1st hidden layer) of randomised weights with number of rows equal to number of columns in the data (x=self.input.shape[1]) and number of columns eqaul to 4 (y=4)
+		self.weights1 = np.random.rand(self.input.shape[1], 4)
+		# Produce a second array (2nd hidden layer) of randomised weights with number of rows equal to 4 (x=4) and number of columns eqaul to 1 (y=1) 
+		self.weights2 = np.random.rand(4,1)
+		# Take the labels y, and assign them to an instance of the y object
+		self.y = y
+		# Make an array, of the same size as the labels y array, of zeros, and assign it to an instance of the ouput object
+		self.output = np.zeros(y.shape)
